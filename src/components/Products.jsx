02f187abd8d1737e658f688017eee5d8de1b2ebf
sjake12 <apostol.sjake12@gmail.com>
2024-09-13 18:36:22 +0800

    updated data sources

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,7 +3,13 @@ import data from "../featured";
 
 function Products() {
   const card = data.map((item) => {
-    return <Card coverImg={item.coverImg} />;
+    return (
+      <Card
+        coverImg={item.coverImg}
+        productName={item.name}
+        description={item.description}
+      />
+    );
   });
   return (
     <section className="mt-40 px-40 bg-eco-500 py-10">
--- src/featured.js
+++ src/featured.js
@@ -2,17 +2,25 @@ export default [
   {
     coverImg:
       "https://plus.unsplash.com/premium_photo-1661629241728-0d43f7a6a406?q=80&w=2028&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product name",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1597817109745-c418f4875230?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product name",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
     coverImg:
       "https://plus.unsplash.com/premium_photo-1664007654124-01d39a71c319?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product name",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1700152516180-4f5c8aaf1d7d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product name",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
 ];
