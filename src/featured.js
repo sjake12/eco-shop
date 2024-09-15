2bec8d8e793e2dcf3ebd1085e26c6d6ad62e1afb
sjake12 <apostol.sjake12@gmail.com>
2024-09-15 21:40:38 +0800

    added sustainability rating for every products

--- src/featured.js
+++ src/featured.js
@@ -4,24 +4,27 @@ export default [
       "https://plus.unsplash.com/premium_photo-1661629241728-0d43f7a6a406?q=80&w=2028&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Reusable Cotton Bags",
     description: "Eco-friendly alternative to plastic bags.",
+    rating: 5,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1597817109745-c418f4875230?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Eco-Friendly Soap Bars",
     description: "Natural ingredients, gentle on skin.",
+    rating: 5,
   },
   {
     coverImg:
       "https://plus.unsplash.com/premium_photo-1664007654124-01d39a71c319?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Stainless Steel Water Bottles",
     description: "Durable, BPA-free, easy to clean.",
+    rating: 5,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1700152516180-4f5c8aaf1d7d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Bamboo Toothbrushes",
     description: "Biodegradable, compostable, gentle.",
+    rating: 5,
   },
 ];
-
--- src/items.js
+++ src/items.js
@@ -4,72 +4,83 @@ export default [
       "https://plus.unsplash.com/premium_photo-1661340972130-8653042af866?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Bamboo Straws",
     description: "Eco-friendly alternative to plastic straws.",
+    rating: 4,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1557687790-902ede7ab58c?q=80&w=1996&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Recycled Paper",
     description: "Reduce waste with eco-friendly notebooks.",
+    rating: 4,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1599709513153-f17b8381793a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Bamboo Toothbrushes",
     description: "Biodegradable bamboo toothbrushes.",
+    rating: 4,
   },
   {
     coverImg:
       "https://plus.unsplash.com/premium_photo-1661340972130-8653042af866?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Cloth Diapers",
     description: "Eco-friendly cloth diapers reduce waste.",
+    rating: 3,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1642505172461-f16770343568?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Steel Water Bottles",
     description: "Eco-friendly stainless steel water bottles.",
+    rating: 4,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1624377225030-f0bb343eaa4d?q=80&w=2065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Cleaning Products",
     description: "Make the switch to eco-friendly cleaning products.",
+    rating: 4,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1624377225030-f0bb343eaa4d?q=80&w=2065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Beeswax Wraps",
     description: "Eco-friendly beeswax food wraps replace plastic wrap.",
+    rating: 4,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1642505171672-8900ce695aba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Cotton Bags",
     description: "Reusable cotton bags reduce plastic waste.",
+    rating: 3,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1582615908486-aa0a3958e60e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Solar Chargers",
     description: "Eco-friendly solar powered chargers reduce carbon footprint.",
+    rating: 4,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1624377166852-776794be17f5?q=80&w=2065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Eco-Friendly Yoga Mats",
     description: "Eco-friendly yoga mats reduce waste and toxins.",
+    rating: 4,
   },
   {
     coverImg:
       "https://images.unsplash.com/photo-1589365278144-c9e705f843ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Metal Straws",
     description: "Reusable metal straws reduce plastic waste.",
+    rating: 4,
   },
   {
     coverImg:
       "https://plus.unsplash.com/premium_photo-1661423635728-ebb72749503c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     name: "Soap Bars",
     description: "Eco-friendly soap bars reduce waste and toxins.",
+    rating: 4,
   },
 ];
-
